dcae19606794dd8800738cfd1d9661a20ec04964
shiblu <shiblu.msi@gmail.com>
2023-01-02 16:35:53 +0600

    Some modifications for deployment

--- instagram_app/settings.py
+++ instagram_app/settings.py
@@ -135,7 +135,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+CSRF_TRUSTED_ORIGINS = ['https://web-production-dfb8.up.railway.app']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
